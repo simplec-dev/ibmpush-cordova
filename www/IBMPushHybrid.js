82c6490ea87de201a4cbe0708ab519619b3299e0
System Administrator <root@hocotate.austin.ibm.com>
2014-06-24 15:31:17 -0500

    build changes

--- www/IBMPushHybrid.js
+++ www/IBMPushHybrid.js
@@ -5,7 +5,7 @@
  *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
  *
  *  IBM Mobile Cloud Services, 
- *  IBMPush Service JavaScript SDK v1.0.0.20140624-1454
+ *  IBMPush Service JavaScript SDK v1.0.0.20140624-1520
  *
  */
 
@@ -530,7 +530,7 @@ define('ibm/mobile/service/_IBMPush', ['require','exports','module'],function (r
 
 var logger = IBMLogger.getLogger();
   var IBMPush = {
-      VERSION: "1.0.0.20140624-1454",
+      VERSION: "1.0.0.20140624-1520",
       _push: null,
       initializeService: function (req) {
         logger.info("IBMPush: initializing version: " + this.getVersion());
